033f4fe5de963f4bdbe90de3022bb6191e9157d2
Elcuyfpv <carpio.ja@hotmail.com>
2026-08-03 16:37:08 -0400

    Load enhanced section styling

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const enhancements = document.createElement('link');
+enhancements.rel = 'stylesheet';
+enhancements.href = 'enhancements.css';
+document.head.appendChild(enhancements);
+
 document.getElementById('year').textContent = new Date().getFullYear();
